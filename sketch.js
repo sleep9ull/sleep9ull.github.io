2800b1aa2c9518c4d3cf44438076d162eeb40980
sleep9ull <sleep9ull@gmail.com>
2022-06-26 19:25:19 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,28 +1,28 @@
 var x;
-var y = 200;
+var y = 400;
 var w = 5;
 var h;
 var angle = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(244);
+  background(240);
   rectMode(CENTER);
   for (i = 0; i < 3; i++) {
     for (j = 1; j < 12; j++) {
-      x = j * 30 + i * 10;
+      x = j * 30 + i * 10 + 200;
       h = map(sin(angle + j * 0.2 + i * 1.2), -1, 1, -350, 350);
       if (h < 0) {
-        fill(180);
+        fill(23, 27, 33);
         noStroke();
         h = abs(h);
         rect(x, y, w, h);
         angle += 0.0006;
       } else {
-        fill(82, 164, 78);
+        fill(108, 208, 100);
         noStroke();
         rect(x, y, w, h);
         angle += 0.0006;
@@ -30,3 +30,6 @@ function draw() {
     }
   }
 }
+
+
+var colors = ['rgb(23, 27, 33)', 'rgb(31, 67, 43)', 'rgb(46, 107, 56)', 'rgb(82, 164, 78)', 'rgb(108, 208, 100)']
