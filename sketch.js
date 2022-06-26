0c1e3e52afde1c36852e23acaf8512c9506f3b24
sleep9ull <sleep9ull@gmail.com>
2022-06-26 08:22:16 +0800

    alter project

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var x;
+var y = 200;
+var w = 5;
+var h;
+var angle = 0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(244);
+  rectMode(CENTER);
+  for (i = 0; i < 3; i++) {
+    for (j = 1; j < 12; j++) {
+      x = j * 30 + i * 10;
+      h = map(sin(angle + j * 0.2 + i * 1.2), -1, 1, -350, 350);
+      if (h < 0) {
+        fill(180);
+        noStroke();
+        h = abs(h);
+        rect(x, y, w, h);
+        angle += 0.0006;
+      } else {
+        fill(82, 164, 78);
+        noStroke();
+        rect(x, y, w, h);
+        angle += 0.0006;
+      }
+    }
+  }
+
+}
