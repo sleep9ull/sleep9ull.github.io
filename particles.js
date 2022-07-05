d888f602fa1e6f572a72f81504491b7437674c2f
sleep9ull <sleep9ull@gmail.com>
2022-07-05 21:14:25 +0800

    update project

---
+++ particles.js
@@ -0,0 +1,65 @@
+class Particle {
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = p5.Vector.random2D();
+    this.acc = createVector(0, 0);
+    this.maxspeed = 2;
+    this.h = 0;
+
+    this.prevPos = this.pos.copy();
+  }
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  follow(vectors) {
+    var x = floor(this.pos.x / scl);
+    var y = floor(this.pos.y / scl);
+    var index = x + y * cols;
+    var force = vectors[index];
+    this.applyForce(force);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  show() {
+    stroke(0, 20)
+    // stroke(this.h, 255, 255, 255);
+    // this.h = this.h + 0.5;
+    // if (this.h > 255) {
+    //   this.h = 0;
+    // }
+    strokeWeight(0.5);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y);
+    this.updatePrev();
+  }
+
+  updatePrev() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
+  }
+
+  edges() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,32 +1,59 @@
-var x;
-var y = 200;
-var w = 5;
-var h;
-var angle = 0;
+var inc = 0.1;
+var scl = 10;
+var cols, rows;
+var fr;
+
+var particles = [];
+
+var flowfield;
+
+var zoff = 0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+  // colorMode(HSB, 255);
+  background(255)
+
+  cols = floor(width / scl);
+  rows = floor(height / scl);
+  fr = createP('');
+
+  flowfield = new Array(cols * rows);
+
+  for (let i = 0; i < 300; i++) {
+    particles[i] = new Particle();
+  }
 }
 
+
 function draw() {
-  background(244);
-  rectMode(CENTER);
-  for (i = 0; i < 3; i++) {
-    for (j = 1; j < 12; j++) {
-      x = j * 30 + i * 10;
-      h = map(sin(angle + j * 0.2 + i * 1.2), -1, 1, -350, 350);
-      if (h < 0) {
-        fill(180);
-        noStroke();
-        h = abs(h);
-        rect(x, y, w, h);
-        angle += 0.0006;
-      } else {
-        fill(82, 164, 78);
-        noStroke();
-        rect(x, y, w, h);
-        angle += 0.0006;
-      }
+  var yoff = 0;
+  for (var y = 0; y < rows; y++) {
+    var xoff = 0;
+    for (var x = 0; x < cols; x++) {
+      var index = x + y * cols;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 2;
+      var v = p5.Vector.fromAngle(angle);
+      v.setMag(0.7);
+      flowfield[index] = v;
+      xoff += inc;
+      // stroke(0, 80);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // strokeWeight(1);
+      // line(0, 0, scl, 0);
+      // pop();
     }
+    yoff += inc;
+    zoff += 0.0002;
   }
+
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].follow(flowfield);
+    particles[i].update();
+    particles[i].edges();
+    particles[i].show();
+  }
+  fr.html(floor(frameRate()));
 }
