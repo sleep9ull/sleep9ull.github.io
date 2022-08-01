409722cdb29c977b0739337643f682b853d0adda
sleep9ull <sleep9ull@gmail.com>
2022-08-02 00:36:53 +0800

    update

--- sketch.js
+++ sketch.js
@@ -1,32 +1,33 @@
-let noiseMax = 1.5;
-let zoff = 0;
+var x, y;
+let walker;
 
 function setup() {
-  let cnv = createCanvas(400, 400);
-  cnv.parent('sketch');
+  createCanvas(400, 400);
+  walker = new Walker(200, 200);
+  background(51);
 }
 
 function draw() {
-  background(250);
-  translate(width / 2, height / 2);
-  stroke(200);
-  strokeWeight(0.4);
+  walker.update();
+  walker.show();
+}
 
-  for (let b = 0; b < TWO_PI; b += TWO_PI / 12) {
 
-    beginShape();
-    for (let a = 0; a < TWO_PI; a += 0.01) {
-      let color = map(b, 0, 10, 0, 255);
-      fill(0, color, 255, 8);
-      let xoff = map(cos(a + b), -1, 1, 0, noiseMax);
-      let yoff = map(sin(a + b), -1, 1, 0, noiseMax);
-      let r = map(noise(xoff, yoff, zoff), 0, 1, 80, 200);
-      let x = r * cos(a);
-      let y = r * sin(a);
-      vertex(x, y);
-    }
-    endShape(CLOSE);
-
+class Walker {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(1, 0);
   }
 
+  update() {
+    this.pos.add(this.vel);
+    // this.pos.x = this.pos.x + this.vel.x;
+    // this.pos.y = this.pos.y + this.vel.y;
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    point(this.pos.x, this.pos.y);
+  }
 }
