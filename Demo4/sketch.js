6e17986fe1429bbf4d81149f9f45c6e47ca9a796
sleep9ull <sleep9ull@gmail.com>
2022-08-06 19:42:47 +0800

    update

--- Demo4/sketch.js
+++ Demo4/sketch.js
@@ -3,7 +3,7 @@ let zoff = 0;
 
 function setup() {
   let cnv = createCanvas(400, 400);
-  cnv.parent('sketch');
+  cnv.parent("sketch");
 }
 
 function draw() {
@@ -13,11 +13,10 @@ function draw() {
   strokeWeight(0.4);
 
   for (let b = 0; b < TWO_PI; b += TWO_PI / 12) {
-
     beginShape();
     for (let a = 0; a < TWO_PI; a += 0.01) {
       let color = map(b, 0, 10, 0, 255);
-      fill(255, 255, color, 8);
+      fill(100, 255, color, 8);
       let xoff = map(cos(a + b), -1, 1, 0, noiseMax);
       let yoff = map(sin(a + b), -1, 1, 0, noiseMax);
       let r = map(noise(xoff, yoff, zoff), 0, 1, 80, 200);
@@ -26,9 +25,7 @@ function draw() {
       vertex(x, y);
     }
     endShape(CLOSE);
-
   }
   // phase += 0.1;
   zoff += 0.02;
-
 }
