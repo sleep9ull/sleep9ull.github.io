27720361653b4178e59ea665c34e4b245f47a253
sleep9ull <sleep9ull@gmail.com>
2022-08-07 18:38:30 +0800

    adding demo6

---
+++ Demo6/sketch.js
@@ -0,0 +1,46 @@
+let pi;
+let digits = [];
+let index = 0;
+let diff = 2;
+
+function preload() {
+  pi = loadStrings("pi1000000.txt");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(24);
+  background(0);
+  stroke(255);
+  strokeWeight(0.5);
+  noFill();
+  translate(width / 2, height / 2);
+  ellipse(0, 0, 400, 400);
+
+  const piString = pi[0];
+  const sdigits = piString.split("");
+  digits = int(sdigits);
+
+  // console.log(piString.length);
+  // console.log(digits.length);
+}
+
+function draw() {
+  translate(width / 2, height / 2);
+  let digit = digits[index];
+  let nextDigit = digits[index + 1];
+  index++;
+
+  let a1 = map(digit, 0, 10, 0, TWO_PI) + random(-diff, diff);
+  let a2 = map(nextDigit, 0, 10, 0, TWO_PI) + random(-diff, diff);
+
+  let x1 = 200 * cos(a1);
+  let y1 = 200 * sin(a1);
+
+  let x2 = 200 * cos(a2);
+  let y2 = 200 * sin(a2);
+
+  strokeWeight(0.5);
+  stroke(random(128, 192), random(128, 192), random(128, 192));
+  bezier(x1, y1, 0, 0, 0, 0, x2, y2);
+}
