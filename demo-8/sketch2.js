c45f8f9bdf29b4ae2e4ddd74e4f306101d125b7d
sleep9ull <sleep9ull@gmail.com>
2022-11-01 19:10:19 +0800

    update

--- demo-8/sketch2.js
+++ demo-8/sketch2.js
@@ -1,93 +1,93 @@
-var rectangles = [];
-var img;
+var rectangles = []
+var img
 
 function preload() {
-  img = loadImage("assets/MonaLisa.jpg");
+  img = loadImage("assets/MonaLisa.jpg")
 }
 
 function setup() {
-  const cnv = createCanvas(300, 450);
-  cnv.parent("sketch");
+  const cnv = createCanvas(300, 450)
+  cnv.parent("sketch")
 
-  var density = displayDensity();
-  pixelDensity(1);
-  img.loadPixels();
+  var density = displayDensity()
+  pixelDensity(1)
+  img.loadPixels()
 
-  console.log(img.width);
-  console.log(img.height);
-  console.log("pixels", img.pixels.length);
-  console.log(density);
+  console.log(img.width)
+  console.log(img.height)
+  console.log("pixels", img.pixels.length)
+  console.log(density)
 }
 
 function draw() {
-  background(255);
+  background(255)
   // frameRate(30);
 
-  var total = 20;
-  var count = 0;
-  var attempts = 0;
+  var total = 20
+  var count = 0
+  var attempts = 0
 
   while (count < total) {
-    var newS = newRectangle();
+    var newS = newRectangle()
     if (newS !== null) {
-      rectangles.push(newS);
-      count++;
+      rectangles.push(newS)
+      count++
     }
-    attempts++;
+    attempts++
     if (attempts > 10000) {
-      noLoop();
-      console.log("finished");
-      break;
+      noLoop()
+      console.log("finished")
+      break
     }
   }
 
   for (rectangle of rectangles) {
     if (rectangle.growing) {
       if (rectangle.edges()) {
-        rectangle.growing = false;
+        rectangle.growing = false
       } else {
         for (other of rectangles) {
           if (rectangle !== other) {
-            let dX = Math.abs(rectangle.x - other.x);
-            let dY = Math.abs(rectangle.y - other.y);
-            let distance = (rectangle.side + other.side) / 2;
+            let dX = Math.abs(rectangle.x - other.x)
+            let dY = Math.abs(rectangle.y - other.y)
+            let distance = (rectangle.side + other.side) / 2
 
             if (dX - 2 < distance && dY - 2 < distance) {
-              rectangle.growing = false;
+              rectangle.growing = false
               // console.log("not growing -2");
-              break;
+              break
             }
           }
         }
       }
     }
 
-    rectangle.show();
-    rectangle.grow();
+    rectangle.show()
+    rectangle.grow()
   }
 }
 
 function newRectangle() {
-  let x = random(0, img.width);
-  let y = random(0, img.height);
+  let x = random(0, img.width)
+  let y = random(0, img.height)
 
-  let valid = true;
+  let valid = true
   for (rectangle of rectangles) {
-    let d = dist(x, y, rectangle.x, rectangle.y);
+    let d = dist(x, y, rectangle.x, rectangle.y)
     if (d - 2 < rectangle.side / 2) {
-      valid = false;
-      break;
+      valid = false
+      break
     }
   }
 
   if (valid) {
-    let index = (int(x) + int(y) * img.width) * 4;
-    let r = img.pixels[index];
-    let g = img.pixels[index + 1];
-    let b = img.pixels[index + 2];
-    let c = color(`rgba(${r}, ${g}, ${b}, 0.8)`);
-    return new Rectangle(x, y, c);
+    let index = (int(x) + int(y) * img.width) * 4
+    let r = img.pixels[index]
+    let g = img.pixels[index + 1]
+    let b = img.pixels[index + 2]
+    let c = color(`rgba(${r}, ${g}, ${b}, 0.8)`)
+    return new Rectangle(x, y, c)
   } else {
-    return null;
+    return null
   }
 }
